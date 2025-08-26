442bfbbbda870523001ffa1b961ad82b0d01b89e
divyalakshmi0 <divyalachu13@gmail.com>
2025-08-26 11:39:01 +0530

    Added

--- src/main/java/com/example/demo_service/config/CorsConfig.java
+++ src/main/java/com/example/demo_service/config/CorsConfig.java
@@ -9,10 +9,14 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1.0/**")
-                .allowedOrigins("https://applogics.vercel.app")
+                .allowedOrigins(
+                        "https://applogics.vercel.app",
+                        "https://b86389ed-4c01-4b96-b966-7eb99b586dd6-00-3qsfgfmccjso.sisko.replit.dev"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
-}
+}d
 
 
